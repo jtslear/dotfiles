dfaec87857e9c3cbbc901820cde62afc57972ce5
John T Skarbek <jskarbek@gitlab.com>
2019-08-01 08:11:30 -0400

    dump

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 for output in $(xrandr | grep '\Wconnected' | awk {'print $1'}); do
-  if [[ $output =~ ^HDMI-2$ ]]
+  if [[ $output =~ ^HDMI2$ ]]
   then
-    xrandr --output HDMI-2 --mode 3440x1440 --primary --output eDP-1 --off
+    xrandr --output HDMI2 --auto --set audio on
+    xrandr --output HDMI2 --auto --set audio on --mode 3440x1440 --primary --output eDP1 --off
+    sleep 2
+    killall dunst
+    notify-send 'monitor-setup'
   else
-    xrandr --output eDP-1 --mode 2048x1152 --primary
+    xrandr --output eDP1 --mode 1600x900 --primary
+    sleep 2
+    killall dunst
+    notify-send 'monitor-setup'
   fi
 done
 
