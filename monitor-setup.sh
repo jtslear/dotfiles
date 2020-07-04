1292aaaa0823abaea6993d3dfa560148a10f2229
John T Skarbek <jskarbek@gitlab.com>
2020-07-04 16:46:25 -0400

    various updates that ive added overtime without any context cuz i suck at updating this on a regular basis

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -5,15 +5,11 @@ for output in $(xrandr | grep '\Wconnected' | awk {'print $1'}); do
   then
     xrandr --output HDMI2 --auto --set audio on
     xrandr --output HDMI2 --auto --set audio on --mode 3440x1440 --primary --output eDP1 --off
-    sleep 2
-    killall dunst
-    notify-send 'monitor-setup'
   else
     xrandr --output eDP1 --mode 1600x900 --primary
-    sleep 2
-    killall dunst
-    notify-send 'monitor-setup'
   fi
 done
 
 setxkbmap -option caps:escape
+
+echo cmd:restart > /tmp/polybar_mqueue.$(pidof polybar)
